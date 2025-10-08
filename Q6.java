e2270ff1d48bfa7b3057ce9b7966c987cf447da1
ThaoDujardin <thaodujardin@gmail.com>
2025-10-08 16:32:36 +0200

    difference Q6

--- Q6.java
+++ Q6.java
@@ -10,14 +10,13 @@ public class Q6
 		int n = Integer.parseInt(args[0]);
 		StringBuilder sb = new StringBuilder();
 
-		// commentaire de difference
-
 		for (int i = n; i >= 1; i--)
 		{
 			sb.setLength(0);
 			for (int j = 0; j < i; j++)
 			{
 				sb.append( 'x' );
+				sb.append(' ');
 				sb.append(' ');
 			}
 			System.out.println(sb.toString());
