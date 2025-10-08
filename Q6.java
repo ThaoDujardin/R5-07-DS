233fc184445c162a23c5da395f062ec7f56b0f80
ThaoDujardin <thaodujardin@gmail.com>
2025-10-08 16:09:50 +0200

    Q6

---
+++ Q6.java
@@ -0,0 +1,24 @@
+public class Q6
+{
+	public static void main(String[] args)
+	{
+		if (args.length < 1)
+		{
+			return;
+		}
+
+		int n = Integer.parseInt(args[0]);
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = n; i >= 1; i--)
+		{
+			sb.setLength(0);
+			for (int j = 0; j < i; j++)
+			{
+				sb.append( 'x' );
+				sb.append(' ');
+			}
+			System.out.println(sb.toString());
+		}
+	}
+}
